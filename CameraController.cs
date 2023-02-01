f652749697cd026b80b51d819ac01ac0c2b5e725
EVGENY_SE <74773570+evgeny2020se@users.noreply.github.com>
2023-02-01 16:07:40 +0300

    Add files via upload

---
+++ CameraController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private float mouseX;
+    private float mouseY;
+
+    [Header("Чувствительность мыши")]
+    public float sentivityMouse = 500f;
+
+    public Transform Player;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X") * sentivityMouse * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * sentivityMouse * Time.deltaTime;
+
+        Player.Rotate(mouseX * new Vector3(0, 1, 0));
+        transform.Rotate(-mouseY * new Vector3(1, 0, 0));
+    }
+}
